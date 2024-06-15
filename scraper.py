77612d1b08aac0b85890c361ae479527b320e5b2
Filip Dvorak <dvof01@vse.cz>
2024-06-15 22:08:53 +0200

    fungujici image fr fr

--- scraper.py
+++ scraper.py
@@ -7,8 +7,6 @@ import time
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-
-
 def getPhoneNumber(Referer, idi, idPhone):
     response = io.BytesIO()
     c = pycurl.Curl()
@@ -29,23 +27,25 @@ def getPhoneNumber(Referer, idi, idPhone):
                             'Sec-Fetch-Mode: cors',
                             'Sec-Fetch-Site: same-origin',
                             'User-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299'])
+    # Set CA bundle path explicitly
+    c.setopt(pycurl.CAINFO, '/etc/ssl/certs/ca-certificates.crt')
     c.perform()
     c.close()
 
     compressed_data = response.getvalue()
     response.close()
 
-    # Dekompresování dat
+    # Decompress data
     decompressed_data = gzip.decompress(compressed_data)
     return decompressed_data
-
-#getPhoneNumber('https://auto.bazos.cz/inzerat/187008051/skoda-fabia-3-fc-10tsi-70kw-combi-cr-dph-style-sport.php')
 
 def getListingsUrls(pageNumber):
     response = io.BytesIO()
     c = pycurl.Curl()
     c.setopt(c.URL, 'https://auto.bazos.cz/' + pageNumber)
     c.setopt(c.WRITEFUNCTION, response.write)
+    # Set CA bundle path explicitly
+    c.setopt(pycurl.CAINFO, '/etc/ssl/certs/ca-certificates.crt')
     c.perform()
     c.close()
 
@@ -67,6 +67,8 @@ def getIdsFromListings(listings):
         c = pycurl.Curl()
         c.setopt(c.URL, url)
         c.setopt(c.WRITEFUNCTION, response.write)
+        # Set CA bundle path explicitly
+        c.setopt(pycurl.CAINFO, '/etc/ssl/certs/ca-certificates.crt')
         c.perform()
         c.close()
 
@@ -79,27 +81,22 @@ def getIdsFromListings(listings):
         ids.append(id)
     return ids
 
-
-
 Listings = getListingsUrls('20')
 ids = getIdsFromListings(Listings)
 
-
-# Nastavení přístupových práv a autentizace
+# Set up access rights and authentication
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets",
          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 
-creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope) #json file nebudu davat na github ten posle jen vam
+creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope) #json file will not be uploaded to GitHub
 client = gspread.authorize(creds)
 
-# Vytvoření instance google sheet (link na sheet v commit message)
+# Create a Google Sheet instance (link to the sheet in commit message)
 sheet = client.open("BazosPhones").sheet1
-
-
 
 counter = 0
 for listing in Listings:
-    if counter%3 == 0 and counter != 0:
+    if counter % 3 == 0 and counter != 0:
         time.sleep(20)
     print(counter)
     print(listing)
@@ -108,6 +105,6 @@ for listing in Listings:
     row = getPhoneNumber(listing, ids[counter][0], ids[counter][1])  
     print(row)
 
-    #zapsání do google sheet
+    # Write to Google Sheet
     sheet.append_row([str(row)])
     counter += 1
